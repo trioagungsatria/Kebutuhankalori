26339ae6e038f272d055aa33ef4301a9921ffc52
trioagungsatria <trioagungsatria@gmail.com>
2025-05-01 21:30:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,11 +110,6 @@ if st.button("Tampilkan Rekomendasi"):
         unsafe_allow_html=True
     )
 
-    )File "/mount/src/kebutuhankalori/streamlit_app.py", line 113
-)
-^
-SyntaxError: unmatched ')'
-
 
 
 # Slide: Tentang Aplikasi
